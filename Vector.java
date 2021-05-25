44196d3e783feefbb3c2a585c45bc4748f395330
ThisIsNsh <hadanis.singh@gmail.com>
2021-05-25 16:35:17 +0530

    Vector Clock Added

--- Vector.java
+++ Vector.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Lamport {
+public class Vector {
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
@@ -12,9 +12,9 @@ public class Lamport {
 
 		int events[] = new int[p + 1];
 		List<String>[] event_instructions = new ArrayList[p + 1];
-		Map<String, Integer>[] event_map = new HashMap[p + 1];
-		List<Integer>[] response = new ArrayList[p + 1];
-		int[] timestamp = new int[p + 1];
+		Map<String, int[]>[] event_map = new HashMap[p + 1];
+		List<int[]>[] response = new ArrayList[p + 1];
+		int[][] timestamp = new int[p+1][p+1];
 		int max_events = Integer.MIN_VALUE;
 
 		System.out.println("Enter the no of events per process:");
@@ -52,16 +52,36 @@ public class Lamport {
 					check = true;
 					String e = event_instructions[i].get(0);
 					if (e.equals("x")) {
-						response[i].add(++timestamp[i]);
+						++timestamp[i][i];
+
+						int[] cur_time = new int[p+1];
+						System.arraycopy(timestamp[i], 0, cur_time, 0, p+1);
+						
+						response[i].add(cur_time);
 						event_instructions[i].remove(0);
 					} else if (e.charAt(0) == 's') {
-						event_map[i].put(e, ++timestamp[i]);
-						response[i].add(timestamp[i]);
+						++timestamp[i][i];
+
+						int[] cur_time = new int[p+1];
+						System.arraycopy(timestamp[i], 0, cur_time, 0, p+1);
+
+						event_map[i].put(e, cur_time);
+
+						response[i].add(cur_time);
 						event_instructions[i].remove(0);
 					} else if (e.charAt(0) == 'r') {
 						if (event_map[e.charAt(1) - '0'].containsKey("s" + i)) {
-							timestamp[i] = Math.max(timestamp[i], event_map[e.charAt(1) - '0'].get("s" + i));
-							response[i].add(++timestamp[i]);
+							++timestamp[i][i];
+							int[] rec_time = event_map[e.charAt(1) - '0'].get("s" + i);
+							
+							for(int k=1; k<=p; k++) {
+								timestamp[i][k] = Math.max(timestamp[i][k], rec_time[k]);
+							}
+							
+							int[] cur_time = new int[p+1];
+							System.arraycopy(timestamp[i], 0, cur_time, 0, p+1);
+
+							response[i].add(cur_time);
 							event_instructions[i].remove(0);
 							event_map[e.charAt(1) - '0'].remove("s" + i);
 						}
@@ -71,16 +91,23 @@ public class Lamport {
 					}
 				}
 			}
-
 		}
 
-		System.out.println("Logical Clock Value:");
+		System.out.println("Vector Clock Value:");
 		for (i = 1; i <= p; i++) {
 			System.out.print("P" + i + " : ");
 			for (j = 1; j <= events[i]; j++) {
-				System.out.print(response[i].get(j-1) + " ");
+				print(response[i].get(j-1));
+				System.out.print(" ");
 			}
 			System.out.println();
+		}
+	}
+
+	public static void print(int[] arr) {
+		for(int i=1; i<arr.length; i++) {
+			System.out.print(arr[i]);
+			if (i<arr.length-1) System.out.print(",");
 		}
 	}
 }
